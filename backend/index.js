b46e5ce7348aac2492fa44540460fef3daedd075
Joe <joeyballesteros.wfg@gmail.com>
2021-05-07 18:03:51 -0700

    connected to db

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+app.use(express.json());
+app.use(cors());
+const Blog = require('./blog.js')
 
-app.get('/', (req,res) => console.log(res))
+mongoose
+.connect('mongodb+srv://ballemup:X2fuGkB59QIjO2jg@cluster0.4j3oy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err));
 
 
+app.get('/home', (req,res) => console.log(res))
+
+app.post('/', (req,res) => {
+    console.log(req.body)
+    return Blog.create (req.body)
+})
 app.listen(5000, () => console.log('Listening on Port 5000'))
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState('')
+  
+  function handleChange(e) {
+    setMessage(e.target.value);
+    console.log(message)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios.post('http://localhost:5000', {message})
+  }
+
+
   return (
     <div className="App">
-      This is the start
+      <h1>Sign Up Sheet</h1>
+      <form onSubmit={handleSubmit}>
+        <input type='text' onChange={handleChange}/>
+        <button type='submit'> Submit</button>
+      </form>
     </div>
   );
 }
